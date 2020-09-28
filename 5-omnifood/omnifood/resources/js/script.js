0406a888d2064905b100ebace42475f8d9c216a3
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-09-28 17:54:25 -0400

    5-omnifood - building sticky nav - part 2 - example of how to pass handler option as first argument to waypoint

--- 5-omnifood/omnifood/resources/js/script.js
+++ 5-omnifood/omnifood/resources/js/script.js
@@ -2,5 +2,14 @@ $(document).ready(function() {
 
 
 
+    
+/*
+var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+*/
+
 
 });
