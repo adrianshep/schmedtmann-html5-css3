e48d235ee7dce38fdfe4e3a952b07fd743179c83
Adrian Shepelavey <adrianshepelavey@gmail.com>
2020-10-17 16:00:45 -0400

    5-omnifood - catch up script.js

--- 5-omnifood/omnifood/resources/js/script.js
+++ 5-omnifood/omnifood/resources/js/script.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
 
     $(window).resize(function(){
         var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
+        var icon = $('.js--nav-icon i');                                                                                                                                                                                                                                      
         if ($(window).width() > 767){
             nav.css("display", "block");
             icon.addClass('ion-close-round');
